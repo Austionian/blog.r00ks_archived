d9f749e14ed7957ff0735891590878c26c95ed19
Austin <austin@r00ks.io>
2021-12-27 22:34:30 -0600

    fix webpack build

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -1,7 +1,16 @@
+/**
+ * Checks if the theme has already been set or it the browser
+ * has a default to supply.
+ * @returns (bool) True for dark; False for light.
+ */
 export function getTheme() {
-    const windowGlobal = typeof window !== 'undefined' && window;
-    let theme = windowGlobal.localStorage.getItem('theme') || null;
+    let theme = null;
+    // Check if the theme was set already in local storage.
+    if (typeof window !== 'undefined') {
+        theme = localStorage.getItem('theme');
+    }
 
+    // If not in local storage, check for the browser preset.
     if (theme === null) {
         const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
         return prefersDarkScheme.matches;
@@ -13,7 +22,12 @@ export function getTheme() {
     return false;
 }
 
+/**
+ * Sets a theme when explicity choosen in local storage.
+ * @param theme (str) light or dark theme to be set.
+ */
 export function setTheme(theme) {
-    const windowGlobal = typeof window !== 'undefined' && window
-    windowGlobal.localStorage.setItem('theme', theme);
+    if (typeof window !== 'undefined') {
+        localStorage.setItem('theme', theme);
+    }
 }
